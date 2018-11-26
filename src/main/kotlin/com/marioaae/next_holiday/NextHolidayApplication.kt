74f080680cf8fc1152ba83b6960829a063fd566d
Mario Álvarez <marioaae@Marios-MBP.fritz.box>
2018-11-26 21:11:59 +0100

    new project

---
+++ src/main/kotlin/com/marioaae/next_holiday/NextHolidayApplication.kt
@@ -0,0 +1,11 @@
+package com.marioaae.next_holiday
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NextHolidayApplication
+
+fun main(args: Array<String>) {
+    runApplication<NextHolidayApplication>(*args)
+}
